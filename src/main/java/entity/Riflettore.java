17dddb16b52b69dbb2aad501675d7e747d816c9a
viola <vfilippo31@gmail.com>
2025-03-24 09:13:04 +0100

    caricato Riflettore

---
+++ src/main/java/entity/Riflettore.java
@@ -0,0 +1,15 @@
+package entity;
+
+
+public class Riflettore {;
+        private String cablaggio;
+        public Riflettore(String cablaggio) {
+            this.cablaggio = cablaggio;
+        }
+
+        public char rifletti(char c) {
+            int indice = c - 'A';
+            return cablaggio.charAt(indice);
+        }
+    }
+
